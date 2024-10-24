47ae9e926a3ca088bb93674fe3a52f3458899ef7
Eli Wilson <ewilson@vacuumtechnology.com>
2024-10-24 15:22:49 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace TorrconEmulator
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pressSlider = new System.Windows.Forms.TrackBar();
+			this.label1 = new System.Windows.Forms.Label();
+			this.connectButton = new System.Windows.Forms.Button();
+			this.disconnectButton = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pressSlider)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pressSlider
+			// 
+			this.pressSlider.Location = new System.Drawing.Point(54, 188);
+			this.pressSlider.Maximum = 760;
+			this.pressSlider.Name = "pressSlider";
+			this.pressSlider.Size = new System.Drawing.Size(808, 69);
+			this.pressSlider.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.label1.Location = new System.Drawing.Point(253, 105);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(346, 40);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "TorrCon IV Emulator";
+			// 
+			// connectButton
+			// 
+			this.connectButton.Location = new System.Drawing.Point(260, 263);
+			this.connectButton.Name = "connectButton";
+			this.connectButton.Size = new System.Drawing.Size(100, 53);
+			this.connectButton.TabIndex = 2;
+			this.connectButton.Text = "Connect";
+			this.connectButton.UseVisualStyleBackColor = true;
+			this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+			// 
+			// disconnectButton
+			// 
+			this.disconnectButton.Location = new System.Drawing.Point(499, 263);
+			this.disconnectButton.Name = "disconnectButton";
+			this.disconnectButton.Size = new System.Drawing.Size(100, 53);
+			this.disconnectButton.TabIndex = 3;
+			this.disconnectButton.Text = "Disconnect";
+			this.disconnectButton.UseVisualStyleBackColor = true;
+			this.disconnectButton.Click += new System.EventHandler(this.disconnectButton_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.ClientSize = new System.Drawing.Size(912, 369);
+			this.Controls.Add(this.disconnectButton);
+			this.Controls.Add(this.connectButton);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pressSlider);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pressSlider)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TrackBar pressSlider;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button connectButton;
+		private System.Windows.Forms.Button disconnectButton;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TorrconEmulator
+{
+	public partial class Form1 : Form
+	{
+        System.Threading.Thread comThread;
+		bool disconnectClicked = false;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			connectButton.Enabled = true;
+			disconnectButton.Enabled = false;
+		}
+
+		private int GetSliderValue()
+		{
+			return pressSlider.Value;
+		}
+
+		private void communicate()
+		{
+			var s = new SerialPort("COM2", 9600);
+			s.Open();
+			byte[] buffer = new byte[1024];
+			string recd, msg;
+			while(!disconnectClicked)
+			{
+				var str = s.Read(buffer, 0, 2);
+				recd = Encoding.Default.GetString(buffer);
+				Console.WriteLine(recd);
+				if(recd.Contains("*p"))
+				{
+					int sliderVal = 0;
+					sliderVal = (int)pressSlider.Invoke(new Func<int>(() => GetSliderValue()));
+					msg = "p1 " + sliderVal.ToString() + ":p2 0.0";
+				}
+				else if(recd.Contains("*v"))
+				{
+					int sliderVal = 0;
+					sliderVal = (int)pressSlider.Invoke(new Func<int>(() => GetSliderValue()));
+					sliderVal /= 76;
+					msg = "p1 " + sliderVal.ToString() + ":p2 0.0";
+				}
+				else
+				{
+					msg = "error";
+				}
+				s.WriteLine(msg);
+				Console.WriteLine(msg);
+			}
+			disconnectClicked = false;
+			s.Close();
+		}
+
+		private void connectButton_Click(object sender, EventArgs e)
+		{
+			connectButton.Enabled = false;
+			disconnectButton.Enabled = true;
+
+			comThread = new System.Threading.Thread(() => communicate());
+			comThread.Start();
+		}
+
+		private void disconnectButton_Click(object sender, EventArgs e)
+		{
+			connectButton.Enabled = true;
+			disconnectButton.Enabled = false;
+			disconnectClicked = true;
+		}
+	}
+}
